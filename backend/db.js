1fc4529ef2e88b35c11a857effadcbd3c22a89c0
Andrew  Gibb <andrewgibb@Andrews-MacBook-Pro.local>
2025-08-21 21:54:48 +0100

    changed to gtp's recommandation

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
-// db.js
+// backend/db.js
 const { Pool } = require("pg");
-
+require('dotenv').config();
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'db',
-  database: process.env.DB_NAME || 'db',
-  password: process.env.DB_PASSWORD || 'password',
-  port: 5432,
+user: process.env.DB_USER,
+host: process.env.DB_HOST,
+database: process.env.DB_NAME,
+password: process.env.DB_PASSWORD,
+port: 5432,
+ssl: { rejectUnauthorized: false }
 });
-
 module.exports = pool;
